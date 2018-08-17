4c0f975e3347982adb6a0d994605a8742de0eb0b
Jayby18 <jbr.accord@gmail.com>
2018-08-17 18:50:10 +0200

    Minor test

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
-using System.Diagnostic;
+using System.Diagnostics;
 
 namespace panorama_wallpaper_changer
 {
