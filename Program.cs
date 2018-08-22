a113ccfb319abb66d8aeb307582992c9b5255334
Jayby18 <jbr.accord@gmail.com>
2018-08-22 13:03:07 +0200

    Build v1.1.1

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ namespace panorama_wallpaper_changer
     class Program
     {
         //Saved in save file
-        public string currentVersion = @"1.1.0";
+        public string currentVersion = @"1.1.1";
         public int wallpaperAmount;
         public string steamInstallPath; //Folder containing steam.exe
         public string csgoInstallPath; //Folder containing csgo.exe
